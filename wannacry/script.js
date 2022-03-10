56c413a4b38ec3d99be047d4a080d5805dce16c5
Dalde <dalde@disroot.org>
2022-03-10 15:18:29 +0100

    Remove mouse from WannaCry simulator and add 2 games

--- wannacry/script.js
+++ wannacry/script.js
@@ -5,9 +5,5 @@ document.body.style.cursor = 'none';
     setInterval(function(){
 			w = Math.floor(Math.random()*($(window).outerWidth()-0+ 1))-150;
 			h = Math.floor(Math.random()*($(window).outerHeight()-0+ 1))-150;
-			$('.mouse').animate({
-				top	: h+10,
-				left: w+820
-			}, 750);
 			$('body').append('<div class="error" style="top:' + h + 'px; left:' + w + 'px;"></div>');
 	}, 750);
